6a6dba4cdd6b66297ccb1649d3bf47e1aff5baef
doyeon789 <143865473+doyeon789@users.noreply.github.com>
2026-01-09 19:28:17 +0900

    Update 1974. 스도쿠 검증.py

--- D2/1974. 스도쿠 검증.py
+++ D2/1974. 스도쿠 검증.py
@@ -1,31 +1,33 @@
+T = int(input())
 
-sudocu = [list(map(int, input().split())) for _ in range(9)]
-sudocu_R =  [[0]*9 for _ in range(9)]
-sudocu_s = []
-answer = 0
+for t in range(1,T+1):
+    sudoku = [list(map(int, input().split())) for _ in range(9)]
+    sudoku_R =  [[0]*9 for _ in range(9)]
+    sudoku_s = []
+    answer = 1
 
-for i in range(9):
-    for j in range(9):
-        sudocu_R[j][i] = sudocu[i][j]
-#3*3칸 비교
-for i in range(9):
-    for j in range(9):
-        arr = []
-        if i%3==0 and j%3==0:
-            for k in range(3):
-                for l in range(3):
-                    arr.append(sudocu[i+k][j+l])
-            sudocu_s.append(arr)
+    for i in range(9):
+        for j in range(9):
+            sudoku_R[j][i] = sudoku[i][j]
+    #3*3칸 비교
+    for i in range(9):
+        for j in range(9):
+            arr = []
+            if i%3==0 and j%3==0:
+                for k in range(3):
+                    for l in range(3):
+                        arr.append(sudoku[i+k][j+l])
+                sudoku_s.append(arr)
 
-for k in range(9):
-    if len(set(sudocu[k])) != 9:
-        answer = 0
-        break
-    if len(set(sudocu_R[k])) != 9:
-        answer = 0
-        break
-    if len(set(sudocu_s[k])) != 9:
-        answer = 0
-        break
+    for k in range(9):
+        if len(set(sudoku[k])) != 9:
+            answer = 0
+            break
+        if len(set(sudoku[k])) != 9:
+            answer = 0
+            break
+        if len(set(sudoku[k])) != 9:
+            answer = 0
+            break
 
-print(answer)
+    print(f"#{t} {answer}")
