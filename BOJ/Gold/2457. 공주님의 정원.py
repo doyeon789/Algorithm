2cd500a82a69e53d9646ce77dca054e379adf937
doyeon789 <ldyeon789@naver.com>
2026-01-14 18:51:10 +0900

    Refactor bloom counting logic and input handling
    Refactor flower input handling and logic for counting blooms.

--- BOJ/Gold/2457. 공주님의 정원.py
+++ BOJ/Gold/2457. 공주님의 정원.py
@@ -1,29 +1,29 @@
 n = int(input())
-flower = []
-for _ in range(n):
-    start_m, start_d, end_m, end_d = map(int, input().split())
-    flower.append([start_m * 100 + start_d, end_m * 100 + end_d])
+f = [list(map(int, input().split())) for _ in range(n)]
+f.sort()
 
-end_date = 301
-count = 0
+i = 0
+result = 0
+latest_end = (3, 1) 
 
-while (flower):
-    if (end_date >= 1201 or flower[0][0] > end_date):
-        break
+while i < n:
+    sm, sd, em, ed = f[i]
+    if (sm, sd) <= latest_end < (em, ed):
+        max_end = (em, ed)
+        while i < n-1: 
+            nsm, nsd, nem, ned = f[i+1]
+            if latest_end < (nsm, nsd):
+                break
+            if max_end < (nem, ned):
+                max_end = (nem, ned)
+            i += 1
+            
+        result += 1
+        latest_end = max_end
+        
+        if (11, 30) < latest_end:
+            print(result)
+            exit(0)
+    i += 1
 
-    temp_end_date = -1
-
-    for _ in range(len(flower)):
-        if (flower[0][0] <= end_date):
-            if (temp_end_date <= flower[0][1]):
-                temp_end_date = flower[0][1]
-            flower.remove(flower[0])
-        else:
-            break
-
-    end_date = temp_end_date
-    count += 1
-if end_date < 1201:
-    print(0)
-else:
-    print(count)
+print(0)
