3aad71fb0a174b9017f334cad63a0c298eb77454
doyeon789 <ldyeon789@naver.com>
2026-01-15 10:55:02 +0900

    Refactor line segment calculation logic

--- BOJ/Gold/2170번: 선 긋기.py
+++ BOJ/Gold/2170번: 선 긋기.py
@@ -1,21 +1,25 @@
+import sys
+input = sys.stdin.readline
+
 n = int(input())
-
 lines = []
-for _ in range(n):
-    a, b = map(int, input().split())
-    lines.append([a, b])
-lines.sort(key=lambda x : x[0])
+ans = 0
 
-start, end = lines[0][0], lines[0][1]
-answer = 0
-for i in range(1, n):
-    if lines[i][0] <= end and lines[i][1] <= end:
-        continue
-    elif lines[i][0] <= end and lines[i][1] > end:
-        end = lines[i][1]
-    else:
-        answer += end - start
-        start = lines[i][0]
-        end = lines[i][1]
-answer += end-start
-print(answer)
+for _ in range (n) :
+    lines.append(tuple(map(int, input().split())))
+
+lines.sort()
+
+start,end = lines[0]
+
+
+for i in range(1,n) :
+    x,y=lines[i]
+
+    if x <= end :
+        end = max(y, end)
+    else :
+        ans += (end - start)
+        start, end = x, y 
+ans += (end - start)
+print(ans)
