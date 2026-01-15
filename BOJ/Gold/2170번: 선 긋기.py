5e3c0a0c4f567967b628b3fe36171f64c77b952b
doyeon789 <ldyeon789@naver.com>
2026-01-15 10:47:04 +0900

    Update 2170번: 선 긋기.py

--- BOJ/Gold/2170번: 선 긋기.py
+++ BOJ/Gold/2170번: 선 긋기.py
@@ -1,13 +1,18 @@
 n = int(input())
-
-coordinate=[]
-
+coordinate = []
 for i in range(n):
     coordinate.append(list(map(int, input().split())))
 
-paper = [0]*max(max(row) for row in coordinate)
+coordinate.sort()
+merged_length = 0
+start, end = coordinate[0]
 
-for i in range(n):
-    for j in range(coordinate[i][0],coordinate[i][1]):
-        paper[j-1] = 1
-print(sum(paper))
+for i in range(1, n):
+    if coordinate[i][0] <= end:
+        end = max(end, coordinate[i][1])
+    else:
+        merged_length += end - start
+        start, end = coordinate[i]
+merged_length += end - start
+
+print(merged_length)
