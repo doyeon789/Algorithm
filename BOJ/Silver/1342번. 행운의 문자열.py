de220d468c750c94571b75c4fc9ca31c420bb0df
doyeon789 <ldyeon789@naver.com>
2026-01-18 11:25:16 +0900

    Update 1342번. 행운의 문자열.py

--- BOJ/Silver/1342번. 행운의 문자열.py
+++ BOJ/Silver/1342번. 행운의 문자열.py
@@ -1,20 +1,19 @@
-from collections import Counter
-import sys
-
-def backTracking(prev, length):
-    global ans
-    if length == len(string):
-        ans += 1
+s = input()
+ 
+lucky = set()
+visited = [False]*len(s)
+ 
+ 
+def bt(string):
+    if len(string) == len(s):
+        lucky.add(string)
         return
-    
-    for ch in cnt:
-        if cnt[ch] > 0 and ch != prev:
-            cnt[ch] -= 1
-            backTracking(ch, length + 1)
-            cnt[ch] += 1
-
-string = string = sys.stdin.readline().strip()
-cnt = Counter(string)
-ans = 0 
-backTracking('', 0)
-print(ans)
+    for i in range(len(s)):
+        if not visited[i] and (not string or string[-1] != s[i]):
+            visited[i] = True
+            bt(string+s[i])
+            visited[i] = False
+ 
+ 
+bt('')
+print(len(lucky))
