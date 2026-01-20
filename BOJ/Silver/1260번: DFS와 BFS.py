b3ba818c03cf4680e0e899efc09ec8fdac7d8ecc
doyeon789 <ldyeon789@naver.com>
2026-01-20 20:24:52 +0900

    Refactor DFS and BFS in Python
    Refactor DFS and BFS implementation with improved graph initialization and sorting.

--- BOJ/Silver/1260번: DFS와 BFS.py
+++ BOJ/Silver/1260번: DFS와 BFS.py
@@ -1,14 +1,38 @@
-n, m , v = map(int, input().split())
+from collections import deque
 
-graph = [[]*(n+1) for _ in range(n+1)]
-for i in range(m):
-    a,b = map(int, input().split())
+n, m, v = map(int, input().split())
+
+graph = [[] for _ in range(n+1)]
+
+for _ in range(m):
+    a, b = map(int, input().split())
     graph[a].append(b)
     graph[b].append(a)
-    graph[a].sort()
-    graph[b].sort()
-print(graph)
 
-visited1 = [False]*(n+1)
-visited12 = visited1.copy()
+for i in range(1, n+1):
+    graph[i].sort()
 
+visited_dfs = [False] * (n+1)
+visited_bfs = [False] * (n+1)
+
+def dfs(v):
+    visited_dfs[v] = True
+    print(v, end=' ')
+    for next_v in graph[v]:
+        if not visited_dfs[next_v]:
+            dfs(next_v)
+
+def bfs(start):
+    queue = deque([start])
+    visited_bfs[start] = True
+    while queue:
+        v = queue.popleft()
+        print(v, end=' ')
+        for next_v in graph[v]:
+            if not visited_bfs[next_v]:
+                visited_bfs[next_v] = True
+                queue.append(next_v)
+
+dfs(v)
+print()
+bfs(v)
