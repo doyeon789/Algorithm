d3d3ed13cd842a49ad0efd39a71fe5a7f0ad798c
doyeon789 <ldyeon789@naver.com>
2026-01-23 09:52:42 +0900

    Update 11724번. 연결요소의 개수.py

--- BOJ/Silver/11724번. 연결요소의 개수.py
+++ BOJ/Silver/11724번. 연결요소의 개수.py
@@ -1,18 +1,26 @@
-def dfs(start):
+from collections import deque
+import sys
+
+def bfs(start):
+    queue = deque()
+    queue.append(start)
     visited[start] = True
-    for next_v in graph[v]:
-        if not visited[next_v]:
-            dfs(next_v)
+    while queue:
+        v = queue.popleft()
+        for next_v in graph[v]:
+            if not visited[next_v]:
+                visited[next_v] = True
+                queue.append(next_v)
     return
 
 
-Vertex , Edge = map(int, input().split())
+Vertex , Edge = map(int, sys.stdin.readline().split())
 
 visited = [False]*(Vertex+1)
 
 graph = [[] for _ in range(Vertex+1)]
 for _ in range(Edge):
-    a,b = map(int,input().split())
+    a,b = map(int,sys.stdin.readline().split())
     graph[a].append(b)
     graph[b].append(a)
 
@@ -20,7 +28,7 @@ count = 0
 
 for i in range(1,Vertex+1):
     if visited[i] == False:
-        dfs(i)
+        bfs(i)
         count += 1
 
 print(count)
