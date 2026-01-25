830c8009423141235c08974a2ab6e57c4f90663a
doyeon789 <ldyeon789@naver.com>
2026-01-25 21:55:23 +0900

    Update 9663번. N-Queen.py

--- BOJ/Gold/9663번. N-Queen.py
+++ BOJ/Gold/9663번. N-Queen.py
@@ -1,1 +1,26 @@
+import sys
 
+n = int(sys.stdin.readline())
+
+visited = [-1] * n
+
+def check(now_row):
+    for row in range(now_row):
+        if visited[now_row] == visited[row] or now_row - row == abs(visited[now_row]-visited[row]):
+            return False
+    return True
+
+cnt = 0
+def dfs(row):
+    global cnt
+    
+    if row == n:
+        cnt += 1
+    
+    else:
+        for col in range(n):
+            visited[row] = col
+            if check(row):
+                dfs(row + 1)
+dfs(0)
+print(cnt)
