c034d1e214a06c8676f208c7b79968f95b93660c
doyeon789 <ldyeon789@naver.com>
2026-01-25 17:35:11 +0900

    Update 14940번. 쉬운 최단거리.py

--- BOJ/Silver/14940번. 쉬운 최단거리.py
+++ BOJ/Silver/14940번. 쉬운 최단거리.py
@@ -4,15 +4,14 @@ dx = [0, 1, 0, -1]
 dy = [1, 0, -1, 0]
 
 n, m = map(int, input().split())
-map = [list(map(int, input().split())) for _ in range(n)]
+board = [list(map(int, input().split())) for _ in range(n)]
 
-dist = [[-1]*m for _ in range(n)]
+dist = [[-1] * m for _ in range(n)]
 q = deque()
 
-# 시작점 찾기
 for i in range(n):
     for j in range(m):
-        if map[i][j] == 2:
+        if board[i][j] == 2:
             q.append((i, j))
             dist[i][j] = 0
 
@@ -21,10 +20,16 @@ while q:
     for d in range(4):
         nx = x + dx[d]
         ny = y + dy[d]
+
         if 0 <= nx < n and 0 <= ny < m:
-            if map[nx][ny] != 0 and dist[nx][ny] == -1:
+            if board[nx][ny] != 0 and dist[nx][ny] == -1:
                 dist[nx][ny] = dist[x][y] + 1
                 q.append((nx, ny))
 
-for row in dist:
-    print(*row)
+for i in range(n):
+    for j in range(m):
+        if board[i][j] == 0:
+            print(0, end=' ')
+        else:
+            print(dist[i][j], end=' ')
+    print()
