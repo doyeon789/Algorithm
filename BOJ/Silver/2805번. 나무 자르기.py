bd21652cc2d658918e2516a9bd3ee5fc78401507
doyeon789 <ldyeon789@naver.com>
2026-01-26 07:27:43 +0900

    Update 2805번. 나무 자르기.py

--- BOJ/Silver/2805번. 나무 자르기.py
+++ BOJ/Silver/2805번. 나무 자르기.py
@@ -12,9 +12,9 @@ while start <= end:
         if i > mid: 
             total += i - mid
         
-        if total < m:
-            end = mid -1
-        else:
-            result = mid
-            start = mid + 1
+    if total < m:
+        end = mid -1
+    else:
+        result = mid
+        start = mid + 1
 print(result)
