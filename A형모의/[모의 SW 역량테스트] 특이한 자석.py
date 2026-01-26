233a3463e74c1ac838c0caa449968d0c1ee6d69a
doyeon789 <ldyeon789@naver.com>
2026-01-27 07:57:09 +0900

    Implement unique magnet rotation simulation

---
+++ A형모의/[모의 SW 역량테스트] 특이한 자석.py
@@ -0,0 +1,54 @@
+from collections import deque
+
+
+def rotate(index, d):
+    if d == 1:
+        gears[index].appendleft(gears[index].pop())
+    elif d == -1:
+        gears[index].append(gears[index].popleft())
+
+
+K = int(input())
+gears = [deque(map(int, input().split())) for _ in range(4)]
+
+for _ in range(K):
+    n, d = map(int, input().split())
+    n -= 1
+    move = [(n, d)]
+
+    temp = d
+    for i in range(n - 1, -1, -1):
+        print("****")
+        if gears[i][2] != gears[i + 1][6]:
+            temp *= -1
+            move.append((i, temp))
+        else:
+            break
+
+    temp = d
+    for i in range(n + 1, 4):
+        if gears[i][6] != gears[i - 1][2]:
+            temp *= -1
+            move.append((i, temp))
+        else:
+            break
+
+    for idx, direction in move:
+        rotate(idx, direction)
+
+answer = 0
+for i in range(4):
+    if gears[i][0] == 1:
+        answer += 2**i
+
+print(f"#{answer}")
+
+'''
+2
+0 0 1 0 0 1 0 0
+1 0 0 1 1 1 0 1
+0 0 1 0 1 1 0 0
+0 0 1 0 1 1 0 1 
+1 1
+3 -1
+'''
