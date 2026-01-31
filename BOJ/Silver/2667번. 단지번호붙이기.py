d3e34ef574be61150a230815d14354bf85f09203
doyeon789 <ldyeon789@naver.com>
2026-01-31 21:23:18 +0900

    Update 2667번. 단지번호붙이기.py

--- BOJ/Silver/2667번. 단지번호붙이기.py
+++ BOJ/Silver/2667번. 단지번호붙이기.py
@@ -1,39 +1,38 @@
-from collections import deque
-
-def bfs(x,y):
-    visited[i][j] = True
-    q = deque()
-    q.append((x,y))
-    cnt = 1
-    while q:
-        x, y = q.popleft()
-        for d in range(4):
-            nx = x + dx[d]
-            ny = y + dy[d]
-            if 0 <= nx < n and 0 <= ny < n:
-                if village[nx][ny] == 1 and not visited[nx][ny]:
-                    visited[nx][ny] = True
-                    q.append((nx,ny))
-                    cnt += 1
-    return cnt
 n = int(input())
-village = [list(map(int, input().strip())) for _ in range(n)]
 
-cnt_lot_number = []
-
-
-# > v < ^
-dx = [0,1,0,-1]
-dy = [1,0,-1,0]
+map = [list(map(int, input().strip())) for _ in range(n)]
 
 visited = [[False]*n for _ in range(n)]
 
+dx = [0,1,0,-1]
+dy = [1,0,-1,0]
+
+apartment = []
+count = 0
+
+def dfs(i,j):
+    global count 
+    count += 1
+    
+    visited[i][j] = True
+    
+    for d in range(4):
+        nx = i + dx[d]
+        ny = j + dy[d]
+        
+        if 0 <= nx < n and 0 <= ny < n:
+            if visited[nx][ny] == False and map[nx][ny] == 1:
+                dfs(nx,ny)        
+    
+    
 for i in range(n):
     for j in range(n):
-        if village[i][j] == 1 and not visited[i][j]:
-            cnt_lot_number.append(bfs(i,j))
-cnt_lot_number.sort()
+        if visited[i][j] == False and map[i][j] == 1:
+            count = 0
+            dfs(i,j)
+            apartment.append(count)
 
-print(len(cnt_lot_number))
-for house_number in cnt_lot_number:
-    print(house_number)
+print(len(apartment))
+apartment.sort()
+for i in range(len(apartment)):
+    print(apartment[i])
