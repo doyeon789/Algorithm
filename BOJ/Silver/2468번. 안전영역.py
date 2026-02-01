f42b79f3e388eb788072313836a83e2daa7b0392
doyeon789 <ldyeon789@naver.com>
2026-02-02 08:28:33 +0900

    Update 2468번. 안전영역.py

--- BOJ/Silver/2468번. 안전영역.py
+++ BOJ/Silver/2468번. 안전영역.py
@@ -1,40 +1,44 @@
 dx = [0,1,0,-1]
 dy = [1,0,-1,0]
 
-def dfs(i,j,map):
-    map[i][j] = True
+def dfs_stack(si, sj, visited):
+    stack = [(si, sj)]
+    visited[si][sj] = True
 
-    for d in range(4):
-        nx = i + dx[d]
-        ny = j + dy[d]
-        if 0 <= nx < n and 0 <= ny < n:
-            if not map[nx][ny]:
-                dfs(nx,ny,map) 
+    while stack:
+        i, j = stack.pop()
+        for d in range(4):
+            nx = i + dx[d]
+            ny = j + dy[d]
+            if 0 <= nx < n and 0 <= ny < n:
+                if not visited[nx][ny]:
+                    visited[nx][ny] = True
+                    stack.append((nx, ny))
 
 n = int(input())
-h_map = [list(map(int,input().split())) for _ in range(n)]
+h_map = [list(map(int, input().split())) for _ in range(n)]
 
-value = set()
-value.add(0)
+value = {0}
 for row in h_map:
     value.update(row)
-
 
 result = 0
 for h in value:
     ans = 0
-    map = [[False]*n for _ in range(n)]
+    visited = [[False]*n for _ in range(n)]
+
     for i in range(n):
         for j in range(n):
             if h_map[i][j] <= h:
-                map[i][j] = True
-    
+                visited[i][j] = True
+
     for i in range(n):
         for j in range(n):
-            if map[i][j] == 0:
+            if not visited[i][j]:
                 ans += 1
-                dfs(i,j,map)
+                dfs_stack(i, j, visited)
 
     result = max(result, ans)
 
 print(result)
+    
