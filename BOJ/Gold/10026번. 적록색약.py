93936e2b493ef26cebbe8f347515c583cd719ae0
doyeon789 <ldyeon789@naver.com>
2026-02-01 21:29:10 +0900

    Update 10026번. 적록색약.py

--- BOJ/Gold/10026번. 적록색약.py
+++ BOJ/Gold/10026번. 적록색약.py
@@ -4,36 +4,46 @@ picture = [list(input().strip()) for _ in range(n)]
 dx = [0, 1, 0, -1]
 dy = [1, 0, -1, 0]
 
-visited = [[False]*n for _ in range(n)]
-visited_cb = [[False]*n for _ in range(n)]
-
-def dfs(x, y):
+def dfs_stack(x, y, visited):
+    stack = [(x, y)]
     visited[x][y] = True
-    for d in range(4):
-        nx = x + dx[d]
-        ny = y + dy[d]
-        if 0 <= nx < n and 0 <= ny < n:
-            if not visited[nx][ny] and picture[x][y] == picture[nx][ny]:
-                dfs(nx, ny)
 
-def dfs_cb(x, y):
-    visited_cb[x][y] = True
-    for d in range(4):
-        nx = x + dx[d]
-        ny = y + dy[d]
-        if 0 <= nx < n and 0 <= ny < n and not visited_cb[nx][ny]:
-            if picture[x][y] in ('R', 'G') and picture[nx][ny] in ('R', 'G'):
-                dfs_cb(nx, ny)
-            elif picture[x][y] == picture[nx][ny]:
-                dfs_cb(nx, ny)
+    while stack:
+        cx, cy = stack.pop()
 
+        for d in range(4):
+            nx = cx + dx[d]
+            ny = cy + dy[d]
+
+            if 0 <= nx < n and 0 <= ny < n:
+                if not visited[nx][ny] and picture[cx][cy] == picture[nx][ny]:
+                    visited[nx][ny] = True
+                    stack.append((nx, ny))
+
+visited = [[False] * n for _ in range(n)]
 ans = 0
-ans_cb = 0
 
 for i in range(n):
     for j in range(n):
         if not visited[i][j]:
             ans += 1
+            dfs_stack(i, j, visited)
+
+for i in range(n):
+    for j in range(n):
+        if picture[i][j] == 'G':
+            picture[i][j] = 'R'
+
+visited = [[False] * n for _ in range(n)]
+ans_colorblind = 0
+
+for i in range(n):
+    for j in range(n):
+        if not visited[i][j]:
+            ans_colorblind += 1
+            dfs_stack(i, j, visited)
+
+print(ans, ans_colorblind)
             dfs(i, j)
 
 for i in range(n):
