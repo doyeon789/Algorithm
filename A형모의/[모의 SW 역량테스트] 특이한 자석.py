d720de31534f83dd39034b573746aa7d6187d6f1
doyeon789 <ldyeon789@naver.com>
2026-02-02 10:25:35 +0900

    Update [모의 SW 역량테스트] 특이한 자석.py

--- A형모의/[모의 SW 역량테스트] 특이한 자석.py
+++ A형모의/[모의 SW 역량테스트] 특이한 자석.py
@@ -7,41 +7,42 @@ def rotate(index, d):
     elif d == -1:
         gears[index].append(gears[index].popleft())
 
-
-K = int(input())
-gears = [deque(map(int, input().split())) for _ in range(4)]
-
-for _ in range(K):
-    n, d = map(int, input().split())
-    n -= 1
-    move = [(n, d)]
-
-    temp = d
-    for i in range(n - 1, -1, -1):
-        print("****")
-        if gears[i][2] != gears[i + 1][6]:
-            temp *= -1
-            move.append((i, temp))
-        else:
-            break
-
-    temp = d
-    for i in range(n + 1, 4):
-        if gears[i][6] != gears[i - 1][2]:
-            temp *= -1
-            move.append((i, temp))
-        else:
-            break
-
-    for idx, direction in move:
-        rotate(idx, direction)
-
-answer = 0
-for i in range(4):
-    if gears[i][0] == 1:
-        answer += 2**i
-
-print(f"#{answer}")
+T = int(input())
+for tc in range(1,T+1):
+    K = int(input())
+    gears = [deque(map(int, input().split())) for _ in range(4)]
+    
+    for _ in range(K):
+        n, d = map(int, input().split())
+        n -= 1
+        move = [(n, d)]
+    
+        temp = d
+        for i in range(n - 1, -1, -1):
+            print("****")
+            if gears[i][2] != gears[i + 1][6]:
+                temp *= -1
+                move.append((i, temp))
+            else:
+                break
+    
+        temp = d
+        for i in range(n + 1, 4):
+            if gears[i][6] != gears[i - 1][2]:
+                temp *= -1
+                move.append((i, temp))
+            else:
+                break
+    
+        for idx, direction in move:
+            rotate(idx, direction)
+    
+    answer = 0
+    for i in range(4):
+        if gears[i][0] == 1:
+            answer += 2**i
+    
+    print(f"#{tc} {answer}")
 
 '''
 2
