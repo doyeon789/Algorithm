1265e582c6adcdf80d7700dfd93a3a91ce8bd8cd
doyeon789 <ldyeon789@naver.com>
2026-02-03 09:32:54 +0900

    gathering files and rename files

---
+++ SWEA/D2/1974. 스도쿠 검증.py
@@ -0,0 +1,33 @@
+T = int(input())
+
+for t in range(1,T+1):
+    sudoku = [list(map(int, input().split())) for _ in range(9)]
+    sudoku_R =  [[0]*9 for _ in range(9)]
+    sudoku_s = []
+    answer = 1
+
+    for i in range(9):
+        for j in range(9):
+            sudoku_R[j][i] = sudoku[i][j]
+    for i in range(9):
+        for j in range(9):
+            arr = []
+            if i%3==0 and j%3==0:
+                for k in range(3):
+                    for l in range(3):
+                        arr.append(sudoku[i+k][j+l])
+                sudoku_s.append(arr)
+
+    for k in range(9):
+        if len(set(sudoku[k])) != 9:
+            answer = 0
+            break
+        if len(set(sudoku[k])) != 9:
+            answer = 0
+            break
+        if len(set(sudoku[k])) != 9:
+            answer = 0
+            break
+
+    print(f"#{t} {answer}")
+
