08c13b525a8dff31bba6eab5a95c107a2b501335
doyeon789 <ldyeon789@naver.com>
2026-02-19 07:23:52 +0900

    review code combinations

--- Study/itertools combinations.py
+++ Study/itertools combinations.py
@@ -1,3 +1,5 @@
+"""
+
 def combinations(arr,r):
     idx = list(range(r))
 
@@ -18,7 +20,31 @@ def combinations(arr,r):
                 for j in range(i + 1, r):
                     idx[j] = idx[j - 1] + 1                    
                 break
+"""
 
+
+def combinations(arr, r):
+    n = len(arr)
+    idx = list(range(r))
+
+    while True:
+        # 현재 조합 먼저 출력
+        yield tuple(arr[i] for i in idx)
+
+        # 뒤에서부터 증가 가능한 위치 찾기
+        for i in reversed(range(r)):
+            if idx[i] != i + n - r:
+                break
+        else:
+            return  # 더 이상 만들 조합이 없음
+
+        # 해당 위치 증가
+        idx[i] += 1
+
+        # 뒤쪽은 순차적으로 재정렬
+        for j in range(i + 1, r):
+            idx[j] = idx[j - 1] + 1
 
 for i in combinations([1,2,3,4,5],3):
     print(i)
+
