1f0b4a6189c5ec239f3806918b202c8fce71c30a
doyeon789 <ldyeon789@naver.com>
2026-02-19 17:02:00 +0900

    solve N과 M(4)

---
+++ BOJ/Silver/15652번. N과 M(4).py
@@ -0,0 +1,12 @@
+N, M = list(map(int,input().split()))
+result = []
+def dfs(start):
+    if len(result) == M:
+        print(' '.join(map(str,result)))
+        return
+    
+    for i in range(start,N+1):
+        result.append(i)
+        dfs(i)
+        result.pop()
+dfs(1)
