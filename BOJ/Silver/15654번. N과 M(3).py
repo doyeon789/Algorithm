e0f1f4c6da56000c698f0e34893dda164a26ceea
doyeon789 <ldyeon789@naver.com>
2026-02-19 16:51:29 +0900

    solve N과 M(3)

---
+++ BOJ/Silver/15654번. N과 M(3).py
@@ -0,0 +1,12 @@
+N, M = list(map(int,input().split()))
+result = []
+def dfs():
+    if len(result) == M:
+        print(' '.join(map(str,result)))
+        return
+    
+    for i in range(1,N+1):
+        result.append(i)
+        dfs()
+        result.pop()
+dfs()
