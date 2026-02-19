a49411effbdb3ecf894963426a181429dce0701a
doyeon789 <ldyeon789@naver.com>
2026-02-19 17:52:50 +0900

    solve N과M(10)

---
+++ BOJ/Silver/15664번. N과 M(10).py
@@ -0,0 +1,23 @@
+N, M = map(int, input().split())
+arr = list(map(int, input().split()))
+arr.sort()
+
+result = []
+
+def dfs(start):
+    if len(result) == M:
+        print(*result)
+        return
+    
+    prev = 0
+    
+    for i in range(start, N):
+        if arr[i] != prev:
+            result.append(arr[i])
+            prev = arr[i]
+            
+            dfs(i + 1)
+            
+            result.pop()
+
+dfs(0)
