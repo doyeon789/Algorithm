3800771f9e55af29acde0bd649c4ca3728f61a36
doyeon789 <ldyeon789@naver.com>
2026-02-19 15:58:32 +0900

    solve N과 M(1)

---
+++ BOJ/Silver/15649번. N과 M(1).py
@@ -0,0 +1,13 @@
+N, M = list(map(int,input().split()))
+result = []
+def dfs():
+    if len(result) == M:
+        print(' '.join(map(str,result)))
+        return
+    
+    for i in range(1,N+1):
+        if i not in result:
+            result.append(i)
+            dfs()
+            result.pop()
+dfs()
