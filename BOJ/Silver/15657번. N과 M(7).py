1052b2f378ef383952c476a2ac1972d21d3102cc
doyeon789 <ldyeon789@naver.com>
2026-02-19 17:39:13 +0900

    solve N과 M(7)

---
+++ BOJ/Silver/15657번. N과 M(7).py
@@ -0,0 +1,17 @@
+N, M = list(map(int,input().split()))
+arr = list(map(int, input().split()))
+arr.sort()
+
+result = []
+def dfs(start):
+    if len(result) == M:
+        for i in result:
+            print(arr[i-1], end = ' ')
+        print()
+        return
+    
+    for i in range(start,N+1):
+        result.append(i)
+        dfs(start)
+        result.pop()
+dfs(1)
