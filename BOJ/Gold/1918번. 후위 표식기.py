ed3502e9875a7c05db243ff78d6ebc20b253e4c6
doyeon789 <ldyeon789@naver.com>
2026-03-02 00:46:38 +0900

    solve 후위 표식기

---
+++ BOJ/Gold/1918번. 후위 표식기.py
@@ -0,0 +1,30 @@
+arr = list(input())
+
+stack=[]
+result=''
+
+for s in arr:
+    if s.isalpha():
+        result+=s
+    else:
+        if s == '(':
+            stack.append(s)
+            
+        elif s == '*' or s == '/':
+            while stack and (stack[-1] == '*' or stack[-1] =='/'):
+                result += stack.pop()
+            stack.append(s)
+            
+        elif s == '+' or s == '-':
+            while stack and stack[-1] != '(':
+                result+= stack.pop()
+            stack.append(s)
+            
+        elif s == ')':
+            while stack and stack[-1] != '(':
+                result += stack.pop()
+            stack.pop()
+            
+while stack :
+    result+=stack.pop()
+print(result)
