d7efd3ed57080cc99abfa2830a8f52e472075a78
doyeon789 <ldyeon789@naver.com>
2026-03-03 07:12:02 +0900

    solve 트리 지름

---
+++ BOJ/Gold/1167번. 트리의 지름.py
@@ -0,0 +1,29 @@
+import sys
+sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
+
+def dfs(x, dist):
+    for i, w in graph[x]:
+        if visited[i] == -1:
+            visited[i] = dist + w
+            dfs(i, dist + w)
+
+N = int(input())
+graph = [[] for _ in range(N+1)]
+for _ in range(N):
+    tree = list(map(int, input().split()))
+    for i in range(1,len(tree)//2):
+        graph[tree[0]].append((tree[2*i-1], tree[2*i]))
+
+visited = [-1] * (N+1)
+visited[1] = 0
+
+dfs(1, 0)
+max_dist = max(visited)
+max_node = visited.index(max_dist)
+
+visited = [-1] * (N+1)
+visited[max_node] = 0 
+dfs(max_node, 0)
+
+print(max(visited))
