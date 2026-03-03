c441c52e540dfc8bf5679d5935b78666632f6de8
doyeon789 <ldyeon789@naver.com>
2026-03-04 08:23:12 +0900

    solve 정수 삼각형

---
+++ BOJ/Silver/1932번. 정수 삼각형.py
@@ -0,0 +1,14 @@
+N = int(input())
+triangle = [list(map(int, input().split())) for _ in range(N)]
+
+floor = len(triangle)-1
+
+for i in range(1, len(triangle)):
+    for j in range(len(triangle[i])):
+        if j == 0:
+            triangle[i][j] += triangle[i-1][0]
+        elif j == len(triangle[i])-1:
+            triangle[i][j] += triangle[i-1][-1]
+        else:
+            triangle[i][j] += max(triangle[i-1][j], triangle[i-1][j-1])
+print(max(triangle[-1]))
