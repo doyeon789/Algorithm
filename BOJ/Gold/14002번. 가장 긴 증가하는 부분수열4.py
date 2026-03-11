7941d9ea8b87581e6dc6fe2c8d5f7e4da26bd921
doyeon789 <ldyeon789@naver.com>
2026-03-12 08:20:45 +0900

    solve 가장 긴 증가하는 부분수열4

---
+++ BOJ/Gold/14002번. 가장 긴 증가하는 부분수열4.py
@@ -0,0 +1,24 @@
+n = int(input())
+arr = list(map(int,input().split()))
+
+dp = [1]*n
+prev = [-1]*n
+
+for i in range(n):
+    for j in range(i):
+        if arr[j] < arr[i] and dp[j] + 1 > dp[i]:
+            dp[i] = dp[j] + 1
+            prev[i] = j
+
+length = max(dp)
+idx = dp.index(length)
+
+result = []
+while idx != -1:
+    result.append(arr[idx])
+    idx = prev[idx]
+
+result.reverse()
+
+print(length)
+print(*result)
